2cea8c04300c73a49804bb737d8af92a060d9b0f
hannmx <mxkud@mail.ru>
2022-11-13 17:26:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGB_HW13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1,10000000);
+            Console.WriteLine("Случайное число: " + number);
+            if (number < 100)
+            {
+                Console.WriteLine("Третьей цифры нет!");
+            }
+            else
+            {
+                bool n = true;
+                while(n)    //сокращаем число до 3-х разрядов
+                {
+                    if (number > 1000)
+                    {
+                        number = number / 10;
+                    }
+                    else
+                    {
+                        n = false; 
+                    }
+                }
+                int result = number - (number / 100) * 100 - (((number % 100) / 10) * 10);
+                Console.WriteLine("Третья цифра заданного числа равна: " + result);
+            }
+        }
+    }
+}
